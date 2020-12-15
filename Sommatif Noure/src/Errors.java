faf01274ccd61b24b1ffbc95ed93e6fe1ff24725
noure-d-zobiri <noure-d-zobiri@192.168.0.154>
2020-12-15 15:26:39 -0500

    Mon sommative, jem'excuse qu'il est un peut en retard j'ai eu quelque problem pour le push

---
+++ Sommatif Noure/src/Errors.java
@@ -0,0 +1,29 @@
+
+/*
+*Author : noure-d-zobiri
+*Date : Dec. 15, 2020
+*Description des erreurs:
+*1.Vous avez changer le nom du public class sans avoir renommer l'unite de compliation
+*2.Vous ne pouvez pas faire une reference static avec une methode non-statique
+*3.Vous devez ecrire le nom de la personne dans la methode saluerAmi
+*4.Vous devez ecrire "Bonjour ami" dans le public static void main
+*/ 
+
+public class Errors
+	{
+
+		public static void main(String[] args)
+			{
+			System.out.println("Bonjour mon ami:");}		
+				/*
+		 * Methode permettant de saluer un ami en
+		 * utilisant son nom
+		 */
+ public String saluerAmi(String nom)
+ {
+		 String message=nom;
+		 System.out.println("John");
+ }
+
+	
+	}
---
+++ Sommatif Noure/src/Methode.java
@@ -0,0 +1,59 @@
+
+/*
+*Author : noure-d-zobiri
+*Date : Dec. 15, 2020
+*/
+import java.util.Scanner;
+
+public class Methode
+	{
+
+		public static void main(String[] args)
+			{
+				//Methode pour genere 100 nombre par hasard dans le tableau
+			int []myTab= new int[100];
+			for(int i=0;i<10;i++)
+			      
+			{
+				myTab[i]=(int)
+						(Math.random()*(i+1));
+			
+			}
+	
+			for (int element:myTab); 
+			
+			}
+			static void int PrimeNum(int []myTab) {
+						for(int i=0; i<myTab.length;i++) {
+							boolean isPrime = true;	
+							
+							for(int j=2;j<i;j++) {
+								if(i%j==0) {
+									isPrime=false;
+									break;}
+							}
+						}
+				}
+			
+			static int SommePrime(int []myTab) {
+			int somme=0;
+			
+	         for(int m=0; m<myTab.length;m++) 
+			 {
+				 somme=somme+ myTab[m];
+			 }
+			}
+	}
+			
+			
+			
+		
+			
+  
+	  
+		  
+		  
+  
+		
+
+	
